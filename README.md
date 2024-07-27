8d1c8d10a810ff41195cdb9b35929fdf37977f11
Nicolas Giai <nicogiai@gmail.com>
2024-07-27 10:18:26 -0300

    doxygen}+

---
+++ README.md
@@ -0,0 +1,8 @@
+# foobar library
+
+This is an example
+
+```bash
+$ ./example 12345
+  54321
+```
--- include/foobar/reverse.h
+++ include/foobar/reverse.h
@@ -1,6 +1,32 @@
+/**
+ * @file reverse.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-07-26
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
 #ifndef FOOBAR_REVERSE_H
 #define FOOBAR_REVERSE_H
 
-int reverse(int);
+/**
+ * @brief Reverse number
+ * 
+ * This function receives an integer number and it returns
+ * same number in the reverse order.
+ * 
+ * Example:
+ * 
+ * [in]  12345
+ * 
+ * [out] 54321
+ * 
+ * @param[in] number  Number to be reversed
+ * 
+ * @return int  Reversed number
+ */
+int reverse(int number);
 
 #endif
